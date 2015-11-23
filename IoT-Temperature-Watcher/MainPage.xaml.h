7278f03c33dc8d6934566db0e631927cbea0db1f
Arcticon <arcticonhardstyle@gmail.com>
2015-11-23 10:14:41 +0100

    * Initial Project

---
+++ IoT-Temperature-Watcher/MainPage.xaml.h
@@ -0,0 +1,26 @@
+﻿//
+// MainPage.xaml.h
+// Deklaration der MainPage-Klasse
+//
+
+#pragma once
+
+#include "MainPage.g.h"
+
+namespace IoT_Temperature_Watcher
+{
+	/// <summary>
+	/// Eine leere Seite, die eigenständig verwendet werden kann oder auf die innerhalb eines Rahmens navigiert werden kann.
+	/// </summary>
+	public ref class MainPage sealed
+	{
+	public:
+		MainPage();
+
+	private:
+		void toggledToggleSwitchCustomMode(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+		void toggledToggleSwitchVentilator1(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+		void toggledToggleSwitchVentilator2(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+		void clickedButtonStartServer(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+	};
+}
