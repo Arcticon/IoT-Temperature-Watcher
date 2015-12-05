215471fa4ff46126e06fe8841f43b775e56f3269
Arcticon <arcticonhardstyle@gmail.com>
2015-12-06 00:10:21 +0100

    * 06.12.15 00:10 Maxi

--- Arduino/TCPServer/TCPServer.ino
+++ Arduino/TCPServer/TCPServer.ino
@@ -3,433 +3,444 @@
 #include <OneWire.h>
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
- 
-// set the LCD address to 0x3F for a 20 chars 4 line display
-// Set the pins on the I2C chip used for LCD connections:
-//                    addr, en,rw,rs,d4,d5,d6,d7,bl,blpol
+
+/*
+ * LCD Display
+ */
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 
-byte mac[] = {
+/*
+ * Set variables for the TCP connection
+ */
+byte macAdress[] = {
   0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
 };
 IPAddress ip(10,11,90,231);
-IPAddress myDns(10,10,10,10);
+IPAddress dnsServer(10,10,10,10);
 IPAddress gateway(10, 11,90,254);
 IPAddress subnet(255, 255, 255, 0);
 
-const long port = 50001;
+/*
+ * Set Temperature Sensors
+ */
+OneWire temperatureSensor1(2);
+OneWire temperatureSensor2(3);
+OneWire temperatureSensor3(4);
+OneWire temperatureSensor4(5);
+OneWire temperatureSensor5(6);
+OneWire temperatureSensor6(7);
+byte sensorCount = 6;
+bool isSensor1Enabled, isSensor2Enabled, isSensor3Enabled, isSensor4Enabled, isSensor5Enabled, isSensor6Enabled = true;
 
+/* 
+ * temperatureSensor1820 Variables
+ */
+byte i;
+byte present = 0;
+byte data1[12];
+/*byte data1[12];
+byte data1[12];
+byte data1[12];
+byte data1[12];
+byte data1[12];*/
+byte addr1[8];
+/*byte addr1[8];
+byte addr1[8];
+byte addr1[8];
+byte addr1[8];
+byte addr1[8];*/
+
+/*
+ * Temperature variables
+ */
+float celsius = 0;
+float temperature1, temperature2, temperature3, temperature4, temperature5, temperature6 = 0;
+
+/*
+ * Variables for Datacenter, Room & Rack number
+ */
+byte datacenterNumber, roomNumber, rackNumber = 1;
+
+/*
+ * TCP Port number
+ */
+const unsigned int port = 50001;
+
+/*
+ * Set TCP Server and Client
+ */
 EthernetServer server(port);
 EthernetClient client;
 
-// DS1820 Variablen
-byte i;
-byte present = 0;
-byte type_s1 = 0;
-byte type_s2 = 0;
-byte data1[12];
-byte data2[12];
-byte data3[12];
-byte data4[12];
-byte data5[12];
-byte data6[12];
-byte addr1[8];
-byte addr2[8];
-byte addr3[8];
-byte addr4[8];
-byte addr5[8];
-byte addr6[8];
-
-bool ignore = false;
-
-char tmpString[10];
-
-float celsius = 0;
-float tmpFloat1, tmpFloat2, tmpFloat3, tmpFloat4, tmpFloat5, tmpFloat6 = 0;
+char stringBuffer[10];
 
 String str_data = "";
 
-int ventilator1State = LOW;
-int ventilator2State = LOW;
+bool ventilator1State = false;
+bool ventilator2State = false;
 
-int globalI = 0;
+/*
+ * Pins for the Fans
+ */
+byte fan1Pin = 8;
+byte fan2Pin = 9;
 
-OneWire ds1(2);
-OneWire ds2(3);
-OneWire ds3(4);
-OneWire ds4(5);
-OneWire ds5(6);
-OneWire ds6(7);
-
-int relay1 = 8;
-int relay2 = 9;
-
-int upperTemp1 = 34;
-int upperTemp1v2 = 38;
-int lowerTemp1 = 30;
-int upperTemp2 = 32;
-int upperTemp2v1 = 34;
-int lowerTemp2 = 27;
-int upperTemp1v2Test = 0;
-int upperTemp2v1Test = 0;
+/*
+ * Thresholds for the Fans to start
+ */
+byte thresholdUpper1Fan1 = 34;
+byte thresholdUpper1Fan2 = 38;
+byte thresholdLower1 = 30;
+byte thresholdUpper2Fan2 = 32;
+byte thresholdUpper2Fan1 = 34;
+byte thresholdLower2 = 27;
+bool thresholdUpper1Fan2Control = false;
+bool thresholdUpper2Fan1Control = false;
 
 const int sensorDelay = 0;
 
 long currTime = 0;
 long currTime2 = 0;
 
+void setTemperatures();
+
+/*
+ * Function to print Data to the LCD Display
+ */
 void printDataToLcd();
 
 void setup()
 {
   Serial.begin(115200);
+  
   lcd.begin(20,4);
-  
   lcd.setCursor(0, 0);
-  
-  if ( !ds1.search(addr1)) {
-    ds1.reset_search();
-    Serial.println("ERROR #1");
-    return;
-  }
-  if ( !ds2.search(addr2)) {
-    ds2.reset_search();
-    Serial.println("ERROR #2");
-    return;
-  }
-  if ( !ds3.search(addr3)) {
-    ds3.reset_search();
-    Serial.println("ERROR #3");
-    return;
-  }
-  if ( !ds4.search(addr4)) {
-    ds4.reset_search();
-    Serial.println("ERROR #4");
-    return;
-  }
-  if ( !ds5.search(addr5)) {
-    ds5.reset_search();
-    Serial.println("ERROR #5");
-    return;
-  }
-  if ( !ds6.search(addr6)) {
-    ds6.reset_search();
-    Serial.println("ERROR #6");
-    return;
-  }
-  Ethernet.begin(mac, ip, myDns, gateway, subnet);
+
+  Ethernet.begin(macAdress, ip, dnsServer, gateway, subnet);
   server.begin();
 
-  pinMode(relay1, OUTPUT);
-  pinMode(relay2, OUTPUT);
+  pinMode(fan1Pin, OUTPUT);
+  pinMode(fan2Pin, OUTPUT);
 
-  tmpFloat1 = getTemp(1);
-  tmpFloat2 = getTemp(2);
-  tmpFloat3 = getTemp(3);
-  tmpFloat4 = getTemp(4);
-  tmpFloat5 = getTemp(5);
-  tmpFloat6 = getTemp(6);
+  setTemperatures();
 
   printDataToLcd();
 }
 
 void loop() {
   client = server.available();
+  
   if (millis() - currTime >= 500) {
-    tmpFloat1 = getTemp(1);
-    tmpFloat2 = getTemp(2);
-    tmpFloat3 = getTemp(3);
-    tmpFloat4 = getTemp(4);
-    tmpFloat5 = getTemp(5);
-    tmpFloat6 = getTemp(6);
+    setTemperatures();
     currTime = millis();
   }
+  
   control();
+  
   if (millis() - currTime2 >= 10000) {
     printDataToLcd();
     currTime2 = millis();
   }
+  
   if (client) {
     if (client.available() > 0) {
       char c = client.read();
       str_data += c;
     }
+    
     if (client.available() == 0) {
       //Serial.println(str_data);
       String str_split1 = str_data.substring(0, 7);
       int sliderVal = str_data.substring(8, 10).toInt();
       if(str_split1 == "slider1"){
-        upperTemp1 = sliderVal;
+        thresholdUpper1Fan1 = sliderVal;
       }else if(str_split1 == "slider2"){
-        lowerTemp1 = sliderVal;
+        thresholdLower1 = sliderVal;
       }else if(str_split1 == "slider3"){
-        upperTemp2 = sliderVal;
+        thresholdUpper2Fan2 = sliderVal;
       }else if(str_split1 == "slider4"){
-        lowerTemp2 = sliderVal;
+        thresholdLower2 = sliderVal;
       }else if(str_split1 == "slider5"){
-        upperTemp1v2 = sliderVal;
-        upperTemp1v2Test == 1;
+        thresholdUpper1Fan2 = sliderVal;
+        thresholdUpper1Fan2Control == true;
       }else if(str_split1 == "slider6"){
-        upperTemp2v1 = sliderVal;
-        upperTemp2v1Test == 1;
+        thresholdUpper2Fan1 = sliderVal;
+        thresholdUpper2Fan1Control == true;
       }
+      
       if (str_data == "getTemp1") {
         //dtostrf(FLOAT,WIDTH,PRECSISION,BUFFER);
-        server.write(dtostrf(tmpFloat1, 0, 2, tmpString));
+        server.write(dtostrf(temperature1, 0, 2, stringBuffer));
       } else if (str_data == "getTemp2") {
-        server.write(dtostrf(tmpFloat2, 0, 2, tmpString));
+        server.write(dtostrf(temperature2, 0, 2, stringBuffer));
       } else if (str_data == "getTemp3") {
-        server.write(dtostrf(tmpFloat3, 0, 2, tmpString));
+        server.write(dtostrf(temperature3, 0, 2, stringBuffer));
       } else if (str_data == "getTemp4") {
-        server.write(dtostrf(tmpFloat4, 0, 2, tmpString));
+        server.write(dtostrf(temperature4, 0, 2, stringBuffer));
       } else if (str_data == "getTemp5") {
-        server.write(dtostrf(tmpFloat5, 0, 2, tmpString));
+        server.write(dtostrf(temperature5, 0, 2, stringBuffer));
       } else if (str_data == "getTemp6") {
-        server.write(dtostrf(tmpFloat6, 0, 2, tmpString));
+        server.write(dtostrf(temperature6, 0, 2, stringBuffer));
       } else if (str_data == "getVent1") {
-        if (ventilator1State == HIGH) {
+        if (ventilator1State == true) {
           server.write("v1on");
         } else {
           server.write("v1off");
         }
       } else if (str_data == "getVent2") {
-        if (ventilator2State == HIGH) {
+        if (ventilator2State == true) {
           server.write("v2on");
         } else {
           server.write("v2off");
         }
-      } else if (str_data == "customModeOn") {
-        ignore = true;
-      } else if (str_data == "customModeOff") {
-        ignore = false;
-        ventilator1State = LOW;
-        ventilator2State = LOW;
       } else if (str_data == "setVent1On") {
-        //V 1 AN
-        digitalWrite(relay1, HIGH);
-        ventilator1State = HIGH;
+        //Fan 1 ON
+        digitalWrite(fan1Pin, HIGH);
+        ventilator1State = true;
       } else if (str_data == "setVent1Off") {
-        //V 1 AUS
-        digitalWrite(relay1, LOW);
-        ventilator1State = LOW;
+        //Fan 1 OFF
+        digitalWrite(fan1Pin, LOW);
+        ventilator1State = false;
       } else if (str_data == "setVent2On") {
-        //V 2 AN
-        digitalWrite(relay2, HIGH);
-        ventilator2State = HIGH;
+        //Fan 2 ON
+        digitalWrite(fan2Pin, HIGH);
+        ventilator2State = true;
       } else if (str_data == "setVent2Off") {
-        //V 2 AUS
-        digitalWrite(relay2, LOW);
-        ventilator2State = LOW;
+        //Fan 2 OFF
+        digitalWrite(fan2Pin, LOW);
+        ventilator2State = false;
       } else if (str_data == "dc01ro01ra01") {
-        server.write(dtostrf(((tmpFloat1 + tmpFloat2 + tmpFloat3 + tmpFloat4 + tmpFloat5 + tmpFloat6)/6), 0, 2, tmpString));
+        server.write(dtostrf(((temperature1 + temperature2 + temperature3 + temperature4 + temperature5 + temperature6)/sensorCount), 0, 2, stringBuffer));
       }
+      
       str_data = "";
     }
   }
 }
 
-// Read Temperature from DS1820
+// Read Temperature from temperatureSensor1820
 float getTemp(int int_switch) {
   if (int_switch == 1) {
-    ds1.reset();
-    ds1.select(addr1);
-    ds1.write(0x44);
+    temperatureSensor1.reset();
+    temperatureSensor1.select(addr1);
+    temperatureSensor1.write(0x44);
 
     delay(sensorDelay);
 
-    present = ds1.reset();
-    ds1.select(addr1);
-    ds1.write(0xBE);
+    present = temperatureSensor1.reset();
+    temperatureSensor1.select(addr1);
+    temperatureSensor1.write(0xBE);
 
     for ( i = 0; i < 9; i++) {
-      data1[i] = ds1.read();
+      data1[i] = temperatureSensor1.read();
     }
-    int16_t raw = (data1[1] << 8) | data1[0];
-    celsius = (float)raw / 16.0;
   } else if (int_switch == 2) {
-    ds2.reset();
-    ds2.select(addr2);
-    ds2.write(0x44);
+    temperatureSensor2.reset();
+    temperatureSensor2.select(addr1);
+    temperatureSensor2.write(0x44);
 
     delay(sensorDelay);
 
-    present = ds2.reset();
-    ds2.select(addr2);
-    ds2.write(0xBE);
+    present = temperatureSensor2.reset();
+    temperatureSensor2.select(addr1);
+    temperatureSensor2.write(0xBE);
 
     for ( i = 0; i < 9; i++) {
-      data2[i] = ds2.read();
+      data1[i] = temperatureSensor2.read();
     }
-
-    int16_t raw = (data2[1] << 8) | data2[0];
-    celsius = (float)raw / 16.0;
   }else if (int_switch == 3) {
-    ds3.reset();
-    ds3.select(addr3);
-    ds3.write(0x44);
+    temperatureSensor3.reset();
+    temperatureSensor3.select(addr1);
+    temperatureSensor3.write(0x44);
 
     delay(sensorDelay);
 
-    present = ds3.reset();
-    ds3.select(addr3);
-    ds3.write(0xBE);
+    present = temperatureSensor3.reset();
+    temperatureSensor3.select(addr1);
+    temperatureSensor3.write(0xBE);
 
     for ( i = 0; i < 9; i++) {
-      data3[i] = ds3.read();
+      data1[i] = temperatureSensor3.read();
     }
-
-    int16_t raw = (data3[1] << 8) | data3[0];
-    celsius = (float)raw / 16.0;
   }else if (int_switch == 4) {
-    ds4.reset();
-    ds4.select(addr4);
-    ds4.write(0x44);
+    temperatureSensor4.reset();
+    temperatureSensor4.select(addr1);
+    temperatureSensor4.write(0x44);
 
     delay(sensorDelay);
 
-    present = ds4.reset();
-    ds4.select(addr4);
-    ds4.write(0xBE);
+    present = temperatureSensor4.reset();
+    temperatureSensor4.select(addr1);
+    temperatureSensor4.write(0xBE);
 
     for ( i = 0; i < 9; i++) {
-      data4[i] = ds4.read();
+      data1[i] = temperatureSensor4.read();
     }
-
-    int16_t raw = (data4[1] << 8) | data4[0];
-    celsius = (float)raw / 16.0;
   }else if (int_switch == 5) {
-    ds5.reset();
-    ds5.select(addr5);
-    ds5.write(0x44);
+    temperatureSensor5.reset();
+    temperatureSensor5.select(addr1);
+    temperatureSensor5.write(0x44);
 
     delay(sensorDelay);
 
-    present = ds5.reset();
-    ds5.select(addr5);
-    ds5.write(0xBE);
+    present = temperatureSensor5.reset();
+    temperatureSensor5.select(addr1);
+    temperatureSensor5.write(0xBE);
 
     for ( i = 0; i < 9; i++) {
-      data5[i] = ds5.read();
+      data1[i] = temperatureSensor5.read();
     }
-
-    int16_t raw = (data5[1] << 8) | data5[0];
-    celsius = (float)raw / 16.0;
   }else if (int_switch == 6) {
-    ds6.reset();
-    ds6.select(addr6);
-    ds6.write(0x44);
+    temperatureSensor6.reset();
+    temperatureSensor6.select(addr1);
+    temperatureSensor6.write(0x44);
 
     delay(sensorDelay);
 
-    present = ds6.reset();
-    ds6.select(addr6);
-    ds6.write(0xBE);
+    present = temperatureSensor6.reset();
+    temperatureSensor6.select(addr1);
+    temperatureSensor6.write(0xBE);
 
     for ( i = 0; i < 9; i++) {
-      data6[i] = ds6.read();
+      data1[i] = temperatureSensor6.read();
     }
-
-    int16_t raw = (data6[1] << 8) | data6[0];
-    celsius = (float)raw / 16.0;
   }
+
+  int16_t raw = (data1[1] << 8) | data1[0];
+  celsius = (float)raw / 16.0;
+  
   return celsius;
 }
 
 void control() {
+  if (temperature1 > thresholdUpper1Fan1) {
+    //Fan 1 ON
+    digitalWrite(fan1Pin, HIGH);
+    ventilator1State = true;
+  }
+  if (temperature1 > thresholdUpper1Fan2) {
+    //Fan 2 ON
+    digitalWrite(fan2Pin, HIGH);
+    ventilator2State = true;
+  }
+  if (temperature2 > thresholdUpper2Fan2) {
+    //Fan 2 ON
+    digitalWrite(fan2Pin, HIGH);
+    ventilator2State = true;
+  }
+  if (temperature2 > thresholdUpper2Fan1) {
+    //Fan 1 ON
+    digitalWrite(fan1Pin, HIGH);
+    ventilator1State = true;
+  }
 
-  if (!ignore) {
-    if (tmpFloat1 > upperTemp1) {
-      //V 1 AN
-      digitalWrite(relay1, HIGH);
-      ventilator1State = HIGH;
-    }
-    if (tmpFloat1 > upperTemp1v2) {
-      //V 2 AN
-      digitalWrite(relay2, HIGH);
-      ventilator2State = HIGH;
-    }
-    if (tmpFloat2 > upperTemp2) {
-      //V 2 AN
-      digitalWrite(relay2, HIGH);
-      ventilator2State = HIGH;
-    }
-    if (tmpFloat2 > upperTemp2v1) {
-      //V 1 AN
-      digitalWrite(relay1, HIGH);
-      ventilator1State = HIGH;
-    }
+  if (temperature1 < thresholdLower1 && thresholdUpper1Fan2Control) {
+    //Fan 1 OFF
+    digitalWrite(fan1Pin, LOW);
+    ventilator1State = false;
+    //Fan 2 OFF
+    digitalWrite(fan2Pin, LOW);
+    ventilator2State = false;
+    thresholdUpper1Fan2Control = false;
+  }else if(temperature1 < thresholdLower1){
+    //Fan 1 OFF
+    digitalWrite(fan1Pin, LOW);
+    ventilator1State = false;
+    //Fan 2 OFF
+    digitalWrite(fan2Pin, LOW);
+    ventilator2State = false;
+  }
+  if(temperature2 < thresholdLower2 && thresholdUpper2Fan1Control) {
+    //Fan 1 OFF
+    digitalWrite(fan1Pin, LOW);
+    ventilator1State = false;
+    //Fan 2 OFF
+    digitalWrite(fan2Pin, LOW);
+    ventilator2State = false;
+    thresholdUpper2Fan1Control = false;
+  }else if(temperature2 < thresholdLower2){
+    //Fan 1 OFF
+    digitalWrite(fan1Pin, LOW);
+    ventilator1State = false;
+    //Fan 2 OFF
+    digitalWrite(fan2Pin, LOW);
+    ventilator2State = false;
+  }
+}
 
-    if (tmpFloat1 < lowerTemp1 && upperTemp1v2Test == 1) {
-      //V 1 AUS
-      digitalWrite(relay1, LOW);
-      ventilator1State = LOW;
-      //V 2 AUS
-      digitalWrite(relay2, LOW);
-      ventilator2State = LOW;
-      upperTemp1v2Test == 0;
-    }else if(tmpFloat1 < lowerTemp1){
-      //V 1 AUS
-      digitalWrite(relay1, LOW);
-      ventilator1State = LOW;
-      //V 2 AUS
-      digitalWrite(relay2, LOW);
-      ventilator2State = LOW;
-    }
-    if(tmpFloat2 < lowerTemp2 && upperTemp2v1Test == 1) {
-      //V 1 AUS
-      digitalWrite(relay1, LOW);
-      ventilator1State = LOW;
-      //V 2 AUS
-      digitalWrite(relay2, LOW);
-      ventilator2State = LOW;
-      upperTemp2v1Test == 0;
-    }else if(tmpFloat2 < lowerTemp2){
-      //V 1 AUS
-      digitalWrite(relay1, LOW);
-      ventilator1State = LOW;
-      //V 2 AUS
-      digitalWrite(relay2, LOW);
-      ventilator2State = LOW;
-      upperTemp2v1Test == 0;
-    }
-    
+void setTemperatures(){
+  if(isSensor1Enabled){
+    temperature1 = getTemp(1);
+  }
+  if(isSensor2Enabled){
+    temperature2 = getTemp(2);
+  }
+  if(isSensor3Enabled){
+    temperature3 = getTemp(3);
+  }
+  if(isSensor4Enabled){
+    temperature4 = getTemp(4);
+  }
+  if(isSensor5Enabled){
+    temperature5 = getTemp(5);
+  }
+  if(isSensor6Enabled){
+    temperature6 = getTemp(6);
   }
 }
 
 void printDataToLcd(){
+  /*
+   * get dc, room, rack number
+   * 
+   * 
+   * 
+   * 
+   */
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("DC");
   lcd.print(" ");
-  lcd.print("1");
+  lcd.print(datacenterNumber);
   lcd.setCursor(6, 0);
   lcd.print("Room");
   lcd.print(" ");
-  lcd.print("1");
+  lcd.print(roomNumber);
   lcd.setCursor(14, 0);
   lcd.print("Rack");
   lcd.print(" ");
-  lcd.print("1");
+  lcd.print(rackNumber);
 
   //Temperature
-
-  lcd.setCursor(0, 1);
-  lcd.print("STL ");
-  lcd.print(tmpFloat1);
-  lcd.setCursor(0, 2);
-  lcd.print("SCL ");
-  lcd.print(tmpFloat2);
-  lcd.setCursor(0, 3);
-  lcd.print("SBL ");
-  lcd.print(tmpFloat3);
-  lcd.setCursor(11, 1);
-  lcd.print("STR ");
-  lcd.print(tmpFloat4);
-  lcd.setCursor(11, 2);
-  lcd.print("SCR ");
-  lcd.print(tmpFloat5);
-  lcd.setCursor(11, 3);
-  lcd.print("SBR ");
-  lcd.print(tmpFloat6);
+  if(isSensor1Enabled){
+    lcd.setCursor(0, 1);
+    lcd.print("STL ");
+    lcd.print(temperature1);
+  }
+  if(isSensor2Enabled){
+    lcd.setCursor(0, 2);
+    lcd.print("SCL ");
+    lcd.print(temperature2);
+  }
+  if(isSensor3Enabled){
+    lcd.setCursor(0, 3);
+    lcd.print("SBL ");
+    lcd.print(temperature3);
+  }
+  if(isSensor4Enabled){
+    lcd.setCursor(11, 1);
+    lcd.print("STR ");
+    lcd.print(temperature4);
+  }
+  if(isSensor5Enabled){
+    lcd.setCursor(11, 2);
+    lcd.print("SCR ");
+    lcd.print(temperature5);
+  }
+  if(isSensor6Enabled){
+    lcd.setCursor(11, 3);
+    lcd.print("SBR ");
+    lcd.print(temperature6);
+  }
 }
 
